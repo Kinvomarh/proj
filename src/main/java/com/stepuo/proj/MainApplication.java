d6df1517f4664a1e2fa40322a71379359a36096e
Aleksandr Anisimov <Kinvomarh87@gmail.com>
2023-09-04 07:01:37 +0300

    доработка логики проекта

--- src/main/java/com/stepuo/proj/MainApplication.java
+++ src/main/java/com/stepuo/proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepuo.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hellow world!");
+        System.out.println("11111111");
     }
 }
