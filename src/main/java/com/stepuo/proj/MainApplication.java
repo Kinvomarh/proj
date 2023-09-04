6e0e402297982fdeed26ec944c9dc597663df4b7
Aleksandr Anisimov <Kinvomarh87@gmail.com>
2023-09-04 07:12:50 +0300

    изменененна основная логика

--- src/main/java/com/stepuo/proj/MainApplication.java
+++ src/main/java/com/stepuo/proj/MainApplication.java
@@ -6,10 +6,14 @@ public class MainApplication {
         System.out.println("10000001");
         System.out.println("22222222");
         System.out.println("33333333");
+
+        doSomething();
     }
 
 
     public static void doSomething(){
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
