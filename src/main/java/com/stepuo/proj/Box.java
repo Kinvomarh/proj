9a5770883f057587f6952c5aa459e5d56f8e8488
Aleksandr Anisimov <Kinvomarh87@gmail.com>
2023-09-04 07:11:25 +0300

    изменененна основная логика добавлен класс коробки

---
+++ src/main/java/com/stepuo/proj/Box.java
@@ -0,0 +1,13 @@
+package com.stepuo.proj;
+
+public class Box {
+    private int size;
+
+    public int getSize(){
+        return size;
+    }
+
+    public void setSize(int size){
+        this.size = size;
+    }
+}
--- src/main/java/com/stepuo/proj/MainApplication.java
+++ src/main/java/com/stepuo/proj/MainApplication.java
@@ -3,6 +3,13 @@ package com.stepuo.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hellow world!");
-        System.out.println("11111111");
+        System.out.println("10000001");
+        System.out.println("22222222");
+        System.out.println("33333333");
+    }
+
+
+    public static void doSomething(){
+        System.out.println(1);
     }
 }
